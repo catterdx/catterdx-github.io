836723e0b3af937463f8b14102b5b45f7de00790
catterdx <58603950+catterdx@users.noreply.github.com>
2019-12-20 00:32:02 +0800

    up

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 let myImage = document.querySelector('img');
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+let myTitle = document.querySelector('title');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/top.jpg') {
@@ -12,17 +13,21 @@ myImage.onclick = function() {
 function setUserName() {
   let myName = prompt('请输入你的名字。');
   if(!myName || myName === null) {
-    setUserName();
+    alert("请输入你的名字。");
+	setUserName();
   } else {
-  localStorage.setItem('张三', myName);
+  localStorage.setItem('name', myName);
   myHeading.textContent = 'Hello！' + myName;
+  myTitle.textContent = '欢迎' + myName +'!';
   }
 }
-if(!localStorage.getItem('张三')) {
+if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  let storedName = localStorage.getItem('张三');
-  myHeading.textContent = 'Hello！' + storedName;
+  let storedName = localStorage.getItem('name');
+   myHeading.textContent = 'Hello！' + storedName;
+   myTitle.textContent = '欢迎' + storedName +'!';
+ 
 }
 myButton.onclick = function() {
    setUserName();
